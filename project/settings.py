936f9ca926c57c17a7fbb38e5d8046532e998c69
danaelshrbiny10 <https://github.com/danaelshrbiny10/automated-price-comparison.git>
2021-01-13 19:52:53 +0200

    add celery

--- project/settings.py
+++ project/settings.py
@@ -121,3 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
