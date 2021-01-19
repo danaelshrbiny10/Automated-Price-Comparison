3ca3195b090d1419fc616ae3dab879d03a14fe92
danaelshrbiny10 <https://github.com/danaelshrbiny10/automated-price-comparison.git>
2021-01-19 21:24:17 +0200

    update data of table

--- data_of_table.py
+++ data_of_table.py
@@ -37,25 +37,16 @@ conn,cur = open_conn("NAME" , "url")
 # cur.execute("SELECT *  from JUMIA")
 # rows = cur.fetchall()
 # for row in rows:
-#    print ("ID = "), row[0]
 #    print ("SKU = "), row[1]
-#    print ("TITLE = "), row[2]
-#    print ("MANUFACTURE = "), row[3]
-#    print ("CATEGORY = "), row[4]
-#    print ("KEYWORDS = "), row[5]
-#    print ("EAN = "), row[6]
-#    print ("ACTIVE = "), row[7]
-#    print ("LASTPRICE = "), row[8]
-#    print ("PRODUCT_ID = "), row[9]
-#    print ("JUMIA_ID = "), row[10]
-#    print ("RATE = "), row[11], "\n"
+#    print ("NAME = "), row[2]
+#    print ("CATEGORIES = "), row[4]
+#    print ("PRICES = "), row[8]
+#    print ("rating = "), row[11], "\n"
 
 # cur.execute("SELECT * from PRICE_HISTORY")
 # rows = cur.fetchall()
 # for row in rows:
-#    print ("PRODUCT_ID = "), row[0]
-#    print ("DATE = "), row[1]
-#    print ("PRICE = "), row[2], "\n"
+#    print ("PRICES = "), row[0], "\n"
 
 # cur.execute("SELECT * from CATEGORY")
 # rows = cur.fetchall()
